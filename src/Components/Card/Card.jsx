866920c3ef1d26667bfe82fb4bfaa9d23e2228ff
nicolasggdev <nicolasggdev@gmail.com>
2021-12-30 15:29:36 -0500

    Se añadio funcionalidad al boton Search

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,21 +1,22 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 // Styles
 import "./Card.styles.css";
 
 // Components
-import Icon from "../Icon/Icon.jsx"
+import Icon from "../Icon/Icon.jsx";
 
 const Card = () => {
 
     // Hooks
     const [data, setData] = useState({});
     const [isLocation, setIsLocation] = useState(false);
-    const [degrees, setDegrees] = useState([0, "°C"]);
-    const [temperature, setTemperature] = useState ()
-    // const [isLoading, setIsLoading] = useState(true)
+    const [degrees, setDegrees] = useState([null, "°C"]);
+    const [search, setSearch] = useState (" ");
+    // const [isLoading, setIsLoading] = useState(true) // PENDIENTE ELABORAR LOADER
 
+    // PENDIENTE TRASLADAR LOGICA AL CUSTOMHOOK
     useEffect(() => {
         const success = position => {
             const lat = position.coords.latitude;
@@ -24,66 +25,106 @@ const Card = () => {
             axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
             .then(res => {
                 const {data: {name, sys: {country}, wind:{speed}, clouds: {all}, main: {temp_max, pressure} , weather}} = res
-                setTemperature({
-                    temp: Math.round(temp_max - 273.15) // Se usó temp_max porque la propiedad temp no daba con exactitud el clima actual y se convirtio de Kelvin a Celsius (se comparó con diversas apps del clima
-                });
                 setData({
                     name: name,
                     country: country,
                     speed: speed,
                     all: all,
-                    // temp: Math.round(temp_max - 273.15), // Se usó temp_max porque la propiedad temp no daba con exactitud el clima actual y se convirtio de Kelvin a Celsius (se comparó con diversas apps del clima)
+                    temp: Math.round(temp_max - 273.15), // Se usó temp_max porque la propiedad temp no daba con exactitud el clima actual y se convirtio de Kelvin a Celsius (se comparó con diversas apps del clima
                     pressure: pressure,
                     weather: weather[0].description,
                     icon: weather[0].icon,
                 });
-                setIsLocation(true)
+                setIsLocation(true);
+                setDegrees([Math.round(temp_max - 273.15), "°C"]);
             })
-            .catch(err => console.log(err))
+            .catch(err => console.log(err));
         };
         const error = () => setIsLocation(false);
         navigator.geolocation.getCurrentPosition(success, error);
     }, []);
 
     // Variables
-    const {name, country, speed, all, pressure, weather, icon} = data;
+    const {name, country, speed, all, temp, pressure, weather, icon} = data;
 
     // Functions
     const handleMeasure = () => {
         let value = 0;
-        if(degrees[1] === "°F"){
-            value = (temperature * 1.8) + 32;
-            setDegrees([value, "°F"])
+        if(degrees[1] === "°C"){
+            value = Math.round((temp * 1.8) + 32);
+            setDegrees([value, "°F"]);
         } else{
-            value = (temperature - 32) / 1.8;
-            setDegrees([value, "°C"])
-        }
+            value = temp;
+            setDegrees([value, "°C"]);
+        };
+    };
 
-        // const temp = 0;    
-        // degrees[] === "°C" ? setDegrees("°F") : setDegrees("°C")
-    }
+    const customPlace = e => {
+        e.preventDefault();
+        const {target: {form}} = e;
+        let place = form[0].value;
+        const key = "a2deae3d58c54683b6b7aca1419f1278";
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${key}`)
+        .then(res => {
+            const {data: {name, sys: {country}, wind:{speed}, clouds: {all}, main: {temp_max, pressure} , weather}} = res
+            setData({
+                name: name,
+                country: country,
+                speed: speed,
+                all: all,
+                temp: Math.round(temp_max - 273.15), // Se usó temp_max porque la propiedad temp no daba con exactitud el clima actual y se convirtio de Kelvin a Celsius (se comparó con diversas apps del clima
+                pressure: pressure,
+                weather: weather[0].description,
+                icon: weather[0].icon,
+            });
+            setDegrees([Math.round(temp_max - 273.15), "°C"]);
+        })
+        .catch(err => {
+            console.log(err);
+            setSearch("It's necessary to write the place correctly")
+        })
+    };
 
     return (
         <div className='card'>
             <h1>Weather App</h1>
-            {isLocation ? <h2>{name}, {country}</h2> : <h2>Can't access your location</h2>}
+            {isLocation ? (
+                <h2>{name}, {country}</h2>
+                ) : ( <h2>Can't access your location</h2>
+            )}
             <div className='info'>
                 <div className='info_icon'>
                     <Icon icon={icon}/>
-                    <p>{degrees[0]} {degrees[1]}</p>
-                    <button onClick={handleMeasure}><b>Degrees °F/°C</b></button>
+                    <p>
+                        {degrees[0]} {degrees[1]}
+                    </p>
+                    <button onClick={handleMeasure}>
+                        <b>Degrees °F/°C</b>
+                    </button>
                 </div>
                 <div className='info_data'>
-                    <p><b>Weather:</b> {weather}</p>
-                    <p><b>Wind speed:</b> {speed} m/s</p>
-                    <p><b>Clouds:</b> {all} %</p>
-                    <p><b>Pressure: </b>{pressure} hPa</p>
+                    <p>
+                        <b>Weather:</b> {weather}
+                    </p>
+                    <p>
+                        <b>Wind speed:</b> {speed} m/s
+                    </p>
+                    <p>
+                        <b>Clouds:</b> {all} %
+                    </p>
+                    <p>
+                        <b>Pressure: </b>{pressure} hPa
+                    </p>
                 </div>
             </div>    
             <div className='search'>
-                <form>
-                    <input type="text" placeholder="Search your city"/>
-                    <input type="submit" /> {/* PREVENT DEFAULT */}
+                <form method="get">
+                    {search === " " ? (
+                        <input className='search_text' type="text" placeholder="Search your city" required/> 
+                        ) : 
+                        <input className='search_text' type="text" placeholder="Search your city" title={search} required/>
+                    }
+                    <input className='search_submit' type="submit" value="Search" onClick={(e) => customPlace(e)}/>
                 </form>
             </div>    
         </div>
--- src/Components/Icon/Icon.jsx
+++ src/Components/Icon/Icon.jsx
@@ -1,50 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 // Styles
-import "./Icon.styles.css"
+import "./Icon.styles.css";
 
-const Icon = ({icon}) => {
+const Icon = ({icon: weather}) => {
 
     // Hooks
-    const [image, setImage] = useState("");
+    const [image, setImage] = useState(null);
 
     useEffect(() => {
-        img(icon)
-    },[icon])
-
+        const img = param => {
+            if(param === "01d"){
+                setImage("fas fa-sun");
+            } else if(param === "01n"){
+                setImage("fas fa-moon");
+            } else if (param === "02d"){
+                setImage("fas fa-cloud-sun");
+            } else if(param === "02n"){
+                setImage("fas fa-cloud-moon");
+            } else if(param === "03d" || "03n"){
+                setImage("fas fa-cloud");
+            } else if(param === "04d" || "04n"){
+                setImage("fas fa-cloud");
+            } else if(param === "09d" || "09n"){
+                setImage("fas fa-cloud-showers-heavy");
+            } else if(param === "10d"){
+                setImage("fas fa-cloud-sun-rain");
+            } else if(param === "10n"){
+                setImage("fas fa-cloud-moon-rain");
+            } else if(param === "11d" || "11n"){
+                setImage("fas fa-poo-storm");
+            } else if(param === "13d" || "13n"){
+                setImage("far fa-snowflake");
+            } else if(param === "50d" || "50n"){
+                setImage("fas fa-smog");
+            }
+        };
+        img(weather);
+        console.log(weather)
+    },[weather]);
     
-    // TENGO PROBLEMAS CON LA IMG PORQUE NO ESTA CAMBIANDO CORRECTAMENTE CON EL CLIMA
-    const img = param => {
-        if(param === "01d"){
-            setImage("fas fa-sun")
-        } else if(param === "01n"){
-            setImage("fas fa-moon")
-        } else if (param === "02d"){
-            setImage("fas fa-cloud-sun")
-        } else if(param === "02n"){
-            setImage("fas fa-cloud-moon")
-        } else if(param === "03d" || "03n"){
-            setImage("fas fa-cloud")
-        } else if(param === "04d" || "04n"){
-            setImage("fas fa-cloud")
-        } else if(param === "09d" || "09n"){
-            setImage("fas fa-cloud-showers-heavy")
-        } else if(param === "10d"){
-            setImage("fas fa-cloud-sun-rain")
-        } else if(param === "10n"){
-            setImage("fas fa-cloud-moon-rain")
-        } else if(param === "11d" || "11n"){
-            setImage("fas fa-poo-storm")
-        } else if(param === "13d" || "13n"){
-            setImage("far fa-snowflake")
-        } else if(param === "50d" || "50n"){
-            setImage("fas fa-smog")
-        }
-    }
 
     return (
         <div className='container_icon'>
-            <i className={image}></i>
+            <i className={`${image}`}></i>
+            {/* <i className="fas fa-smog"></i> */}
         </div>
     )
 }
